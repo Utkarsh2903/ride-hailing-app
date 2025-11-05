0ae7be962a90391708d3e89a84036f8aff0c0c3b
Utkarsh Agrawal <uagrawal29.ua@gmail.com>
2025-11-05 23:16:38 +0530

    Remove indexes for deleted tenant columns

--- db/migrate/20250103000013_create_tenants.rb
+++ db/migrate/20250103000013_create_tenants.rb
@@ -42,11 +42,9 @@ class CreateTenants < ActiveRecord::Migration[7.1]
 
     add_index :tenants, :slug, unique: true, name: "idx_tenants_slug_unique"
     add_index :tenants, :subdomain, unique: true, where: "subdomain IS NOT NULL", name: "idx_tenants_subdomain_unique"
-    add_index :tenants, :custom_domain, unique: true, where: "custom_domain IS NOT NULL", name: "idx_tenants_domain_unique"
     add_index :tenants, :status, name: "idx_tenants_status"
     add_index :tenants, :region, name: "idx_tenants_region"
     add_index :tenants, :country_code, name: "idx_tenants_country"
-    add_index :tenants, :deleted_at, name: "idx_tenants_deleted_at"
   end
 end
 
